fcf97ae73190166fc4844ba3b145a1416097a1b6
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2024-04-09 12:23:41 +0200

    minor update

--- speech2text/main.py
+++ speech2text/main.py
@@ -1,18 +1,18 @@
 import json
 import os
+import gcp_speech2text as sp2txt
 
-import speech2text as sptxt
+CREDENTIALS_FILE = "../gcp-credential/key.json"
 
 if __name__ == "__main__":
 
-    CREDENTIAL = './key.json'
     # gcs_uri_wav = "gs://talking-dataset/biz-meeting/biz-result-oup-brainstorming-meeting_16k.wav"
     # gcs_uri_wav = "gs://talking-dataset/biz-meeting/biz-result-oup-participating-in-meetings-1_16k.wav"
     # gcs_uri_wav = "gs://talking-dataset/biz-meeting/biz-result-oup-participating-in-meetings-2_16k.wav"
     gcs_uri_wav = "gs://talking-dataset/biz-meeting/biz-result-oup-team-meeting_16k.wav"
 
     # -------------------------------------------------------------------
-    script_conf_pairs, word_tag_pairs = sptxt.speech_to_text(gcs_uri_wav, CREDENTIAL)
+    script_conf_pairs, word_tag_pairs = sp2txt.speech_to_text(gcs_uri_wav, CREDENTIALS_FILE)
 
     # -------------------------------------------------------------------
     with open('script_conf_pairs.json', 'w') as f:
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     print(f"Create Raw Files!\n{filepath1}\n{filepath2}\n")
 
     # -------------------------------------------------------------------
-    transcript = sptxt.generate_transcript_with_tag(word_tag_pairs)
+    transcript = sp2txt.generate_transcript_with_tag(word_tag_pairs)
 
     with open('transcript.json', 'w') as f:
         json.dump(transcript, f, indent=4)
